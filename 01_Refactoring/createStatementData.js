a34b0fe6b0725e02e06375e0bf6ce94f592f6bcf
marvelopers <marvelopers@gmail.com>
2022-04-26 22:31:50 +0900

    다형성을 활용하여 데이터 생성하기

--- 01_Refactoring/createStatementData.js
+++ 01_Refactoring/createStatementData.js
@@ -7,48 +7,8 @@ const createStatementData = (invoice, plays) => {
   return result;
 };
 
-class PerformanceCalculator {
-  constructor(aPerformance, aPlay) {
-    this.performance = aPerformance;
-    this.play = aPlay;
-  }
-
-  get amount() {
-    let result = 0;
-
-    switch (this.play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (this.performance.audience > 30) {
-          thisAmount += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (this.performance.audience > 20) {
-          thisAmount += 1000 + 500 * (this.performance.audience - 20);
-        }
-        thisAmount += 300 * this.performance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    }
-
-    return result;
-  }
-
-  get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === playFor(this.performance).type) {
-      result += Math.floor(this.performance.audience / 5);
-    }
-    return result;
-  }
-}
 const enrichPerformance = (aPerformance) => {
-  const calcuator = new PerformanceCalculator(
+  const calcuator = createPerformanceCalculator(
     aPerformance,
     playFor(aPerformance)
   );
@@ -61,16 +21,59 @@ const enrichPerformance = (aPerformance) => {
 
 const playFor = (aPerformance) => plays[aPerformance.playID];
 
-const amountFor = (aPerformance, play) =>
-  new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-
-const volumeCreditsFor = (aPerformance) =>
-  new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
-
 const totalAmount = (data) =>
   data.performances.reduce((total, p) => total + p.amount, 0);
 
 const totalVolumeCredits = (data) =>
   data.performances.reduce((total, p) => total + p.volumeCredits, 0);
 
+const createPerformanceCalculator = (aPerformance, aPlay) => {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+};
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  get amount() {
+    throw new Error("서브클래스에서 처리하도록 설계되었습니다.");
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 + 500 * (this.performance.audience - 20);
+    }
+    return result;
+  }
+}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+}
 export default createStatementData;
