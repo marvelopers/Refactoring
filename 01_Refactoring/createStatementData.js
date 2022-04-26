9c44fc2ca6b4e4e48ba6b8b0a91fa837f8399254
marvelopers <marvelopers@gmail.com>
2022-04-26 22:15:18 +0900

    다형성을 위해 함수 옮기기

--- 01_Refactoring/createStatementData.js
+++ 01_Refactoring/createStatementData.js
@@ -8,54 +8,64 @@ const createStatementData = (invoice, plays) => {
 };
 
 class PerformanceCalculator {
-  constructor(aPerformance) {
+  constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  get amount() {
+    let result = 0;
+
+    switch (this.play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (this.performance.audience > 30) {
+          thisAmount += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (this.performance.audience > 20) {
+          thisAmount += 1000 + 500 * (this.performance.audience - 20);
+        }
+        thisAmount += 300 * this.performance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === playFor(this.performance).type) {
+      result += Math.floor(this.performance.audience / 5);
+    }
+    return result;
   }
 }
 const enrichPerformance = (aPerformance) => {
-  const calcuator = new PerformanceCalculator(aPerformance);
+  const calcuator = new PerformanceCalculator(
+    aPerformance,
+    playFor(aPerformance)
+  );
   const result = Object.assign({}, aPerformance);
-  result.play = playFor(result);
-  result.amount = amountFor(result);
-  result.volumeCredits = volumeCreditsFor(result);
+  result.play = calcuator.play;
+  result.amount = calcuator.amount;
+  result.volumeCredits = calcuator.volumeCredits;
   return result;
 };
 
 const playFor = (aPerformance) => plays[aPerformance.playID];
 
-const amountFor = (aPerformance, play) => {
-  let result = 0;
+const amountFor = (aPerformance, play) =>
+  new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
 
-  switch (amountFor(perf).type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (aPerformance.audience > 30) {
-        thisAmount += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (aPerformance.audience > 20) {
-        thisAmount += 1000 + 500 * (aPerformance.audience - 20);
-      }
-      thisAmount += 300 * aPerformance.audience;
-      break;
-
-    default:
-      throw new Error(`알 수 없는 장르: ${amountFor(perf).type}`);
-  }
-
-  return result;
-};
-
-const volumeCreditsFor = (aPerformance) => {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === playFor(aPerformance).type) {
-    result += Math.floor(aPerformance.audience / 5);
-  }
-  return result;
-};
+const volumeCreditsFor = (aPerformance) =>
+  new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
 
 const totalAmount = (data) =>
   data.performances.reduce((total, p) => total + p.amount, 0);
