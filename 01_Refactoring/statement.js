4649129a2a33761718e81d70bc62af0aa48413b2
marvelopers <marvelopers@gmail.com>
2022-04-26 21:24:18 +0900

    함수 선언 바꾸기

--- 01_Refactoring/statement.js
+++ 01_Refactoring/statement.js
@@ -37,27 +37,28 @@ const volumeCreditsFor = (aPerformance) => {
   return result;
 };
 
+const usd = (aNumber) =>
+  new Intl.NumberFormat("es-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+
 const statement = (invoice, plays) => {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
-  const format = new Intl.NumberFormat("es-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
 
   return result;
