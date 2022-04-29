16acf78feb31a15a72a908b9fae7b0fee3476397
marvelopers <marvelopers@gmail.com>
2022-04-29 23:17:31 +0900

    함수 인라인하기

--- 06_Refactoring/02_함수인라인하기.ts
+++ 06_Refactoring/02_함수인라인하기.ts
@@ -2,8 +2,21 @@ type Driver = {
   numberOfLateDeliveries: number;
 };
 
+type Customer = {
+  name: string;
+  location: string;
+};
+
 const moreThanFiveLateDeliveries = (driver: Driver) =>
   driver.numberOfLateDeliveries > 5;
 
 const getRating = (driver: Driver) =>
   driver.numberOfLateDeliveries > 5 ? 2 : 1;
+
+const reportLines = (aCustomer: Customer) => {
+  const lines = [];
+  // getherCustomerData(lines, aCustomer.name);
+  lines.push(["name", aCustomer.name]);
+  lines.push(["location", aCustomer.location]);
+  return lines;
+};
