da8c1387b788abbfe89c91aed1c55670080fd494
marvelopers <marvelopers@gmail.com>
2022-05-02 22:42:25 +0900

    변수이름바꾸기

---
+++ 06_Refactoring/07_변수이름바꾸기.ts
@@ -0,0 +1,16 @@
+// let a = height * width;
+// let area = height * width;
+
+let tpHd = "untitled";
+
+let result = +`<h1>${tpHd}</h1>`;
+
+tpHd = obj["articleTitle"];
+
+const title = () => tpHd;
+const getTitle = (arg: string) => (tpHd = arg);
+
+// 상수이름 바꾸기
+// const cpyNm = "애크미 구스베리";
+const companyName = "애크미 구스베리";
+const cpyNm = companyName;
