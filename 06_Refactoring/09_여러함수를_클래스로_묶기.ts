af273f31bfa4f872e7d4a5135de429b8dd0599c2
marvelopers <marvelopers@gmail.com>
2022-05-05 23:59:31 +0900

    함수 클래스 만들기

--- 06_Refactoring/09_여러함수를_클래스로_묶기.ts
+++ 06_Refactoring/09_여러함수를_클래스로_묶기.ts
@@ -1,11 +1,29 @@
-type ReadingType = {};
+type ReadingType = {
+  customer: string;
+  quantity: number;
+  month: number;
+  year: number;
+};
 
-const base = (aReading) => {};
-const taxableCharge = (aReading) => {};
-const calculateBaseCharge = (aReading) => {};
+const baseRate = (month: number, year: number) => {};
+const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+
+const calculateBaseCharge = (aReading: ReadingType) =>
+ 
+
+let reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const baseChargeAmount = calculateBaseCharge(aReading);
 
 class Reading {
   base() {}
   taxableCharge() {}
-  calculateBaseCharge() {}
+
+  get calculateBaseCharge() {
+    baseRate(aReading.month, aReading.year) * aReading.quantity;
+  }
 }
