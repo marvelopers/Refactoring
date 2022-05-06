0973e633575f47d4ea794414d6803fc2112deea2
marvelopers <marvelopers@gmail.com>
2022-05-06 23:31:43 +0900

    여러함수를 변환함수로 묶기

---
+++ 06_Refactoring/10_여러함수를_변환_함수로_묶기.ts
@@ -0,0 +1,44 @@
+type ReadingType = {
+  customer: string;
+  quantity: number;
+  month: number;
+  year: number;
+};
+
+const baseRate = (month: number, year: number) => {};
+
+let reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
+
+class Reading {
+  constructor(data: ReadingType) {
+    this.customer = data.customer;
+    this.quantity = data.quantity;
+    this.month = data.month;
+    this.year = data.year;
+  }
+  get customer() {
+    return this.customer;
+  }
+  get quantity() {
+    return this.quantity;
+  }
+  get month() {
+    return this.month;
+  }
+  get year() {
+    return this.year;
+  }
+
+  taxableCharge() {
+    return Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+  }
+
+  get baseCharge() {
+    return baseRate(this.month, this.year) * this.quantity;
+  }
+}
+
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const taxableCharge = aReading.taxableCharge;
+const baseChargeAmount = aReading.baseCharge;
